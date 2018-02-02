53cbc28a41c80d502f0e9719da5b5e8ef36633d0
Holger Bruch <holger.bruch@systect.de>
2018-02-02 14:24:53 +0100

    Add json import to docker-entrypoint

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -3,9 +3,14 @@
 if [ "$1" = 'photon' ]; then
 	shift 
 	
+	# if index does not yet exist, import from json.dump (if exists) or nominatim
 	if [ ! -d "/photon/photon_data/elasticsearch" ]; then
-		java -Xmx256m -jar photon-$PHOTON_VERSION.jar -nominatim-import -host $NOMINATIM_PORT_5432_TCP_ADDR -port $NOMINATIM_PORT_5432_TCP_PORT -languages $PHOTON_LANGUAGES
-
+		if [ ! -d $JSON_DUMP_FILE ]; then
+			java -Xmx256m -jar photon-$PHOTON_VERSION.jar -json-import -json $JSON_DUMP_FILE -languages $PHOTON_LANGUAGES
+		else 
+			java -Xmx256m -jar photon-$PHOTON_VERSION.jar -nominatim-import -host $NOMINATIM_PORT_5432_TCP_ADDR -port $NOMINATIM_PORT_5432_TCP_PORT -languages $PHOTON_LANGUAGES
+		fi 
+		
 		echo
 		echo 'Photon init process complete; ready for start up.'
 		echo
