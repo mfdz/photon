d7ce9a66343710383d79bbe014eed69fa1b7f31c
Leonard Ehrenfried <mail@leonard.io>
2020-06-02 12:35:32 +0200

    Remove db password

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -25,7 +25,6 @@ if [ "$1" = 'photon' ]; then
   if [ "$AUTOMATIC_UPDATES" = true ] ; then
     # if you start photon with the credentials for the nominatim db you still have to call the /nominatim-endpoint manually
     # in this docker image this happens through a cron job at 5am every day
-    echo $NOMINATIM_DB_PASSWORD
     exec java -jar photon.jar \
       -host      $NOMINATIM_DB_HOST \
       -port      $NOMINATIM_DB_PORT \
